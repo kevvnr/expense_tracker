418b6dbe90e5a893526321df182ff77d2fe1401a
Kevvn <you@example.com>
2026-04-16 11:32:22 -0400

    Finished dark mode

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:expense_tracker/widgets/expenses.dart';
 import 'package:flutter/material.dart';
 
-var kColorScheme = ColorScheme.fromSeed(seedColor: Color.fromARGB(255, 96, 59, 181));
+ColorScheme kColorScheme = ColorScheme.fromSeed(
+  seedColor: Color.fromARGB(255, 96, 59, 181)
+  );
+  var kDarkColorScheme =ColorScheme.fromSeed(brightness: Brightness.dark, 
+  seedColor: const Color.fromARGB(255, 5, 99, 125),);
 void main() {
   runApp( MaterialApp(
     theme: ThemeData(
@@ -28,7 +32,21 @@ void main() {
         ),
     ),
     ),
+    darkTheme: ThemeData.dark().copyWith(
+      colorScheme: kColorScheme,
+    cardTheme: CardThemeData(
+      color: kDarkColorScheme.secondaryContainer,
+      margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+    ),
+    elevatedButtonTheme: ElevatedButtonThemeData(
+      style: ElevatedButton.styleFrom(
+        backgroundColor: kDarkColorScheme.onPrimaryContainer,
+        foregroundColor: kDarkColorScheme.primaryContainer,
+        ),
+    ),
+    ),
+      themeMode: ThemeMode.system,
     home: Expenses()
-  )
+  ),
   );
 }
